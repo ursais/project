a7b16c5acb050ae6e8127a0a27d02ca4d9cbaa72
Khalid <m.khalid@serpentcs.com>
2020-08-17 12:29:29 +0530

    [15.0][MIG]Project -Project Earned Value (cherry picked from commit 14f97b29f70a67e202c8b593aed155fb1890f266) (cherry picked from commit e3d46d4bc17040d211434f8fc1b322871bf5c22a) (cherry picked from commit ec37f0123b60c4ed11513c3b0e626b168dc2f2ac) (cherry picked from commit 1bfd17fb3a9a3ef474a0f5567da9c0759b8478cf) (cherry picked from commit 66e20aafb9a96ef6cc74d2e3114439e5792185cc) (cherry picked from commit 7f9cd6225ce90d159f09a5723b6f640dd985f462)

---
+++ project_earned_value/__manifest__.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2020 Open Source Integrators
+# Copyright (C) 2020 Serpent Consulting Services
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project Earned Value",
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "author": """Open Source Integrators,
+        Serpent Consulting Services,
+        Odoo Community Association (OCA)""",
+    "summary": """This module adds measures to the Task Analysis report and
+        provides a basic Earned Value Analysis on the project overview.""",
+    "category": "Project",
+    "maintainers": ["Open Source Integrators"],
+    "website": "https://github.com/OCA/project",
+    'depends': [
+        "project",
+        "sale_timesheet"
+    ],
+    "data": [
+        "report/project_report_views.xml",
+    ],
+    "qweb": [
+    ],
+    "auto_install": False,
+    "application": False,
+    "installable": True,
+}
---
+++ project_earned_value/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
+* Mohammed Khalid <m.khalid@serpentcs.com>
+* Hardik Suthar <hsuthar@opensourceintegrators.com>
