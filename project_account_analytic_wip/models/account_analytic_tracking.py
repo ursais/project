2760f18dd85d7ac01540ccbc3a03b741020f063d
Daniel Reis <dreis@opensourceintegrators.com>
2021-03-04 17:46:22 +0000

    [FIX] project_account_analytic_wip: refactor for newer Tracking Item compatibility

--- project_account_analytic_wip/models/account_analytic_tracking.py
+++ project_account_analytic_wip/models/account_analytic_tracking.py
@@ -8,26 +8,12 @@ from odoo import api, fields, models
 class AnalyticTrackingItem(models.Model):
     _inherit = "account.analytic.tracking.item"
 
-    project_task_id = fields.Many2one(
-        "project.task", string="Project Task", ondelete="restrict"
+    sale_order_line_id = fields.Many2one(
+        "sale.order.line", string="Order Line", ondelete="set null"
     )
 
+    @api.depends("sale_order_line_id")
     def _compute_name(self):
         super()._compute_name()
-        for tracking in self.filtered("project_task_id"):
-            move = tracking.project_task_id
-            # TODO: raw_material_production_id in project_task...
-            tracking.name = "{} / {}".format(
-                move.raw_material_production_id.display_name,
-                move.product_id.display_name,
-            )
-
-    @api.depends("manual_planned_amount", "project_task_id")
-    def _compute_planned_amount(self):
-        super()._compute_planned_amount()
-        for tracking in self.filtered("project_task_id"):
-            task = tracking.project_task_id
-            sale_line_id = task.sale_line_id
-            qty = sale_line_id.product_uom_qty
-            unit_cost = sale_line_id.product_id.standard_price
-            tracking.manual_planned_amount += qty * unit_cost
+        for tracking in self.filtered("sale_order_line_id"):
+            tracking.name = tracking.sale_order_line_id.display_name
